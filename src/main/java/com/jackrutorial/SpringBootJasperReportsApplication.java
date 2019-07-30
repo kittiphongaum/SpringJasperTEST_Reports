28015b9c7ed0d65d5b84792ac148e9977f85e499
jook <kittiphong95@gmail.com>
2019-07-30 09:05:45 +0700

    commit

---
+++ src/main/java/com/jackrutorial/SpringBootJasperReportsApplication.java
@@ -0,0 +1,14 @@
+package com.jackrutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJasperReportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJasperReportsApplication.class, args);
+	}
+
+}
+
